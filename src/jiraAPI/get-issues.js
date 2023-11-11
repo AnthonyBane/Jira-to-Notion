3e0fb485d33cbb8c5c030b6ed66a21331a830af7
anthony.bane <anthony.william.bane@gmail.com>
2023-11-11 21:29:55 +0000

    Created base Jira query

---
+++ src/jiraAPI/get-issues.js
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+//Gets all issues in a particular project using the Jira Cloud REST API
+async function getIssues(auth, domain, jquery) {
+
+  try {
+
+    const baseUrl = 'https://' + domain + '.atlassian.net';
+
+    const config = {
+      method: 'get',
+      url: baseUrl + '/rest/api/2/search?jql=' + jquery,
+      headers: { 'Content-Type': 'application/json' },
+      auth: auth
+    };
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.log('error: ')
+    console.log(error.response.data.errors)
+  }
+}
+
+module.exports = getIssues;
